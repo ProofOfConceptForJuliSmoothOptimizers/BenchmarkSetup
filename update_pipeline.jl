533bd04fee6bdaeeb71c8683552fd0a0061bbb40
Monssaf Toukal <tmonssaf@gmail.com>
2020-10-16 00:56:17 -0400

    script now adds/updates a file & can delete it too

--- update_pipeline.jl
+++ update_pipeline.jl
@@ -9,7 +9,6 @@ using GitHub
 using Printf
 using JSON
 using Base64
-using SHA
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -21,13 +20,15 @@ function parse_commandline()
         "--repo", "-r"
             help = "The name of the repositories on GitHub"
             arg_type = String
-            default = "KrylovCI"
-         
+            default = "KrylovCI"   
         
     end
 
     return parse_args(s, as_symbols=true)
 end
+
+"""TODO: make sure to put 'branch_name' as an argument in ArgParse and to add it to all
+    function that needs the paramater. Add it as a named parameter""" 
 
 function get_repo(api::GitHub.GitHubWebAPI, org::String, repo_name::String; kwargs...)
     my_params = Dict(:visibility => "all")
@@ -35,55 +36,77 @@ function get_repo(api::GitHub.GitHubWebAPI, org::String, repo_name::String; kwar
     return Repo(GitHub.gh_get_json(api, "/repos/$(org)/$(repo_name)"; params = my_params, kwargs...))
 end
 
-function update_pipeline(api::GitHub.GitHubWebAPI, org::String, path::String; kwargs...)
+function update_file(api::GitHub.GitHubWebAPI, org::String, path::String; kwargs...)
     repositories, pagedata = GitHub.repos(api, org; kwargs...)
+    file = open(path, "r")
+    myparams = Dict(:message => "updating file from remote script: $path", :branch => "develop", :content => base64encode(file))
+    close(file)
+    [GitHub.update_file(api, repo, path; params = myparams , kwargs...) for repo in repositories]
 
-    return [GitHub.update_file(api, repo, path; kwargs...) for repo in repositories]
+    println("Update Complete!")
 end
 
-function update_pipeline(api::GitHub.GitHubWebAPI, org::String, path::String, repo_names::Vector{String}; kwargs...)
+function update_file(api::GitHub.GitHubWebAPI, org::String, path::String, repo_names::Vector{String}; kwargs...)
+    repositories = [get_repo(api, org, repo_name; kwargs...) for repo_name in repo_names]
+    file = open(path, "r")
+    myparams = Dict(:message => "updating file from remote script: $path", :branch => "develop", :content => base64encode(file))
+    close(file)
+    return [GitHub.update_file(api, repo, path; params = myparams, kwargs...) for repo in repositories]
+end
+
+# TODO: make that function work... 
+function delete_file(api::GitHub.GitHubWebAPI, org::String, path::String, repo_names::Vector{String}; kwargs...)
+    # Getting sha of the file if needed:
+    myparams = Dict(:message => "deleting file from remote script: $path", :branch => "develop")
+    # Looking for the git sha1 of the file to delete:
     repositories = [get_repo(api, org, repo_name; kwargs...) for repo_name in repo_names]
 
-    return [GitHub.update_file(api, repo, path; kwargs...) for repo in repositories]
+    for repo in repositories
+        sha_of_file = get_file_sha(api, path, repo)
+        if length(sha_of_file) > 0
+           myparams[:sha] = sha_of_file
+        end 
+        GitHub.delete_file(api, repo, path; params = myparams, kwargs...)
+        println("file at $(path) deleted in $(repo.name)")
+        delete!(myparams, :sha)
+    end
+    println("Deletion Complete!")
 end
 
+function get_file_sha(api::GitHub.GitHubWebAPI , path_to_file::String, repo::Repo, branch_name = "develop"; kwargs...)
+
+    myparams = Dict(:ref => branch_name)
+    remote_file = file(api, repo, path_to_file; params = myparams, kwargs...)
+    return String(remote_file.sha)
+end
 
 function main()
     api = GitHub.DEFAULT_API
-    # TODO: to remove this line after testing, this is the Auth of JSOBot
-    GITHUB_AUTH = "99c2656683ba93a4f3cb2f01494bcd1bcc416545"
     # Need to add GITHUB_AUTH to your .bashrc
-    myauth = GitHub.authenticate(GITHUB_AUTH)
+    myauth = GitHub.authenticate(env["GITHUB_AUTH"])
     # parse the arguments: 
     parsed_args = parse_commandline()
     org = parsed_args[:org]
     repo_names = parsed_args[:repo]
-    # creatinf ile in each repo
+    # creating/updating file in each repo
     repositories, pagedata = GitHub.repos(api, org; auth = myauth)
-    file_name = "test.txt"
-    file = open(file_name, "r")
-    myparams = Dict(:message => "updating file from remote script: $file_name", :branch => "develop", :content => base64encode(file))
-    updated_content = nothing
+    path_to_file = "Jenkinsfile"
+    
+    # To add a file:
+
     if repo_names == "*"
-        updated_content = update_pipeline(api, org, file_name; auth = myauth, params = myparams)
+        update_file(api, org, path_to_file; auth = myauth)
     else
         repo_names = [String(repo_name) for repo_name in split(repo_names)]
-        updated_content = update_pipeline(api, org, file_name, repo_names; auth = myauth, params = myparams)
+        update_file(api, org, path_to_file, repo_names; auth = myauth)
+        println("Update Done!")
     end
-    print(updated_content)
-    # sha = Git.ls
-    # sha = join([string(x, base = 16, pad = 2) for x in sha1(file)])
-    # myparams = Dict(:message => "deleting file from remote script: $file_name", :branch => "develop", :sha => sha)
-    # GitHub.delete_file(api, org, file_name; auth = myauth, params = myparams)
-    close(file)
+
+    # To delete a file: 
+
+    # repo_names = [String(repo_name) for repo_name in split(repo_names)]
+    # delete_file(api, org, path_to_file, repo_names, auth = myauth)
+
 end
+
 main()
-
-
-# cmd = Git.cmd(Cmd(`git ls-files -s test.txt`))
-# sha = Git.run(cmd)
-# open("test.txt", "r") do file
-#     sha = sha1(file)
-#     println(sha)
-#     println(join([string(x, base = 16, pad = 2) for x in sha]))
-# end
