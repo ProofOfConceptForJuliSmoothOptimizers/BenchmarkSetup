7fe0f419b850d65a7f3ea21613f5f544315223bf
Monssaf Toukal <tmonssaf@gmail.com>
2021-01-13 09:57:28 -0500

    Script can now create branches and pull requests.

--- update_pipeline.jl
+++ update_pipeline.jl
@@ -1,5 +1,5 @@
 using Pkg
-bmark_dir = joinpath(@__DIR__, "env/")
+bmark_dir = joinpath(@__DIR__, "env")
 Pkg.activate(bmark_dir)
 Pkg.instantiate()
 
@@ -14,7 +14,7 @@ function parse_commandline()
     s = ArgParseSettings()
     @add_arg_table! s begin
         "--org", "-o"
-            help = "Name of GitHub Organization"
+            help = "Name of GitHub Organization."
             arg_type = String
             default = "ProofOfConceptForJuliSmoothOptimizers"
         "--repo", "-r"
@@ -22,16 +22,23 @@ function parse_commandline()
             arg_type = String
             default = "Krylov.jl"
         "--delete", "-d"
-            help = "specifies if it's a deletion process"
+            help = "specifies if it's a deletion process."
             action = :store_true
-        # TODO: change branch required to true
-        "--branch", "-b"
-            help = "the name of the new branch on which the modifications will be made."
+        "--new_branch", "-b"
+            help = "the name of the new branch on which the modifications will be made. It must be a new branch name."
             arg_type = String   
-            required = false
+            required = true
+        "--base_branch", "-B"
+            help = "The name of an existing branch. 'master' is the default value."
+            default = "master"
         "--file", "-f"
             help = "path to file to update (e.g dir1/dir2/file_to_update.txt). 
                     Make sure to use the '/' delimiter for the path."
+        "--message", "-m"
+            help = "Commit message describing the modification."
+            arg_type = String
+            required = false
+
     end
 
     return parse_args(s, as_symbols=true)
@@ -40,9 +47,9 @@ end
 """TODO: make sure to put 'branch_name' as an argument in ArgParse and to add it to all
     function that needs the paramater. Add it as a named parameter""" 
 
-function update_file(api::GitHub.GitHubWebAPI, path::String, repositories::Vector{Repo}; kwargs...)
+function update_file(api::GitHub.GitHubWebAPI, path::String, repositories::Vector{Repo}, new_branch_name::String, message::String; kwargs...)
     file = open(path, "r")
-    myparams = Dict(:message => "updating file from remote script: $path", :branch => "master", :content => base64encode(file))
+    myparams = Dict(:branch => new_branch_name, :message => message,  :content => base64encode(file))
     close(file)
     for repo in repositories
         sha_of_file = get_file_sha(api, path, repo)
@@ -55,9 +62,9 @@ function update_file(api::GitHub.GitHubWebAPI, path::String, repositories::Vecto
     end
 end
 
-function delete_file(api::GitHub.GitHubWebAPI, path::String, repos::Vector{Repo}; kwargs...)
+function delete_file(api::GitHub.GitHubWebAPI, path::String, repos::Vector{Repo}, new_branch_name::String, message::String; kwargs...)
     # Getting sha of the file if needed:
-    myparams = Dict(:message => "deleting file from remote script: $path", :branch => "master")
+    myparams = Dict(:branch => new_branch_name, :message => message)
     # Looking for the git sha1 of the file to delete:
 
     for repo in repos
@@ -86,9 +93,37 @@ function get_file_sha(api::GitHub.GitHubWebAPI , path_to_file::String, repo::Rep
     return String(remote_file.sha)
 end
 
-function create_branch(api::GitHub.GitHubWebAPI, new_branch_name::String; current_branch_name = "master")
-    url = api.endpoint.uri
+function create_pullrequest(api::GitHub.GitHubWebAPI, repository::Repo, new_branch_name::String, base_branch::String, message::String; kwargs...)
+
+    myparams = Dict(:head => new_branch_name, :base => base_branch, :title => message)
+
+    GitHub.create_pull_request(api, repository; params = myparams, kwargs...)
 end
+
+function create_pullrequests(api::GitHub.GitHubWebAPI, repositories::Vector{Repo}, new_branch_name::String, base_branch::String, message::String; kwargs...)
+    
+    [create_pullrequest(api, repository, new_branch_name, base_branch, message; kwargs...) for repository in repositories]
+end
+
+# TODO: make this method smart enough to detect if the new branch exists already
+function create_branch(api::GitHub.GitHubWebAPI, org::String, repository::Repo, new_branch_name::String, base_branch::String; kwargs...)
+    base_branch_sha = get_branch_sha(api::GitHub.GitHubWebAPI, org::String, repository::Repo, base_branch::String; kwargs...)
+    myparams = Dict(:ref => "refs/heads/$new_branch_name", :sha => base_branch_sha)
+
+    result = GitHub.gh_post_json(api, "/repos/$org/$(repository.name)/git/refs"; params = myparams, kwargs...)
+end
+
+function create_branches(api::GitHub.GitHubWebAPI, org::String, repositories::Vector{Repo}, new_branch_name::String, base_branch::String; kwargs...)
+
+    [create_branch(api, org, repository, new_branch_name, base_branch; kwargs...) for repository in repositories]
+end
+
+function get_branch_sha(api::GitHub.GitHubWebAPI, org::String, repository::Repo, base_branch::String; kwargs...) 
+    base_branch_dict = GitHub.gh_get_json(api, "/repos/$org/$(repository.name)/git/ref/heads/$base_branch"; kwargs...)
+    
+    return base_branch_dict["object"]["sha"]
+end
+
 
 function main()
     api = GitHub.DEFAULT_API
@@ -100,25 +135,41 @@ function main()
     org = parsed_args[:org]
     repo_names = parsed_args[:repo]
     is_delete = parsed_args[:delete]
-    new_branch_name = parsed_args[:branch]
+    new_branch = parsed_args[:new_branch]
+    base_branch = parsed_args[:base_branch]
+    message = parsed_args[:message]
+
+    # Debug:
+
+    new_branch_name = "test-br"
+    file = "dir1/test.txt"
+    is_delete = false
 
     """" TODO: fix path behaviour
     for now, when given a path like this: './dir1/dir2/file', joinpath does not give the right path
     However, giving 'dir1/dir2/file' works just fine.
     """
+    
+    # assigning default value to commit message: 
 
-    path_to_file = joinpath(split(parsed_args[:file], '/')...) 
+    message = isnothing(message) ?  "updating/deleting file from remote script: $path" : message
 
     path_to_file = parsed_args[:file]
     # getting the right repositories given as argument: 
 
     repositories = repo_names == "all" ? GitHub.repos(api, org; auth = myauth)[1] : [repo for repo in GitHub.repos(api, org; auth = myauth)[1] if repo.name in split(repo_names)]
     
+    # creating new branches for each repo.
+    create_branches(api, org, repositories, new_branch_name, base_branch; auth = myauth)
+
     if is_delete
-        delete_file(api, path_to_file, repositories, auth = myauth)
+        delete_file(api, path_to_file, repositories, new_branch, message, auth = myauth)
     else
-        update_file(api, path_to_file, repositories; auth = myauth)
+        update_file(api, path_to_file, repositories, new_branch, message; auth = myauth)
     end
+    
+    # creating pull request 
+    create_pullrequests(api, repositories, new_branch, base_branch, message; auth = myauth)
 end
 
 main()
