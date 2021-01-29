7e6236a75cab76b45ad06b11f28b7b9233faa892
Monssaf Toukal <tmonssaf@gmail.com>
2021-01-29 16:31:15 -0500

    update to add bmark script WIP

--- src/main.jl
+++ src/main.jl
@@ -6,7 +6,6 @@ Pkg.instantiate()
 using ArgParse
 using Git
 using GitHub
-using Printf
 using JSON
 using Base64
 
@@ -18,6 +17,7 @@ include(joinpath(@__DIR__,"branches.jl"))
 include(joinpath(@__DIR__,"pull_requests.jl"))
 include(joinpath(@__DIR__,"utils.jl"))
 include(joinpath(@__DIR__,"webhooks.jl"))
+include(joinpath(@__DIR__, "repositories.jl"))
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -30,9 +30,9 @@ function parse_commandline()
             help = "The name of the repositories on GitHub. To select all repos, simply write : 'all'."
             arg_type = String
             default = "Krylov.jl"
-        "--delete", "-d"
-            help = "boolean that specifies if it's a deletion process."
-            action = :store_true
+        # "--delete", "-d"
+        #     help = "boolean that specifies if it's a deletion process."
+        #     action = :store_true
         "--new_branch", "-b"
             help = "the name of the new branch on which the modifications will be made. It must be a new branch name."
             arg_type = String   
@@ -43,8 +43,9 @@ function parse_commandline()
         "--file", "-f"
             help = "path to file to update (e.g dir1/dir2/file_to_update.txt and not ./dir1/dir2/file_to_update.txt). 
                     Make sure to use the '/' delimiter for the path."
+            required = false
         "--message", "-m"
-            help = "Commit message describing the modification."
+            help = "pull request title"
             arg_type = String
             required = false
         "--webhook", "-w"
@@ -65,7 +66,6 @@ function main()
     parsed_args = parse_commandline()
     org = parsed_args[:org]
     repo_names = parsed_args[:repo]
-    is_delete = parsed_args[:delete]
     new_branch_name = parsed_args[:new_branch]
     base_branch_name = parsed_args[:base_branch]
     message = parsed_args[:message]
@@ -74,29 +74,17 @@ function main()
 
     # assigning default value to commit message: 
 
-    message = isnothing(message) ?  "updating/deleting files from remote script: $path" : message
+    message = (isnothing(message) || "") ?  "setting up benchmarks" : message
 
     # getting the right repositories given as argument: 
 
     repositories = repo_names == "all" ? GitHub.repos(api, org; auth = myauth)[1] : [repo for repo in GitHub.repos(api, org; auth = myauth)[1] if repo.name in split(repo_names)]
     
-    file_paths = get_file_paths(path)
-
-    # create branches
-    [create_branch(api, org, repository, new_branch_name, base_branch_name; auth = myauth) for repository in repositories]
+    setup_benchmarks(api, org, repositories, new_branch_name, base_branch_name, message; auth = myauth)
     # create webhooks for benchmarks
     if is_webhook
         [create_benchmark_webhook(api, org, repository; auth = myauth) for repository in repositories]
     end
-    # updating or deleting files given in path
-    if is_delete
-        [delete_file(api, file_path, repositories, new_branch_name, message, auth = myauth) for file_path in file_paths]
-    else
-        [update_file(api, file_path, repositories, new_branch_name, message; auth = myauth) for file_path in file_paths]
-    end
-    
-    # creating pull request and branches if needed
-    create_pullrequests(api, org, repositories, new_branch_name, base_branch_name, message; auth = myauth)
 end
 
 main()
---
+++ src/repositories.jl
@@ -0,0 +1,62 @@
+const bmark_dependencies = ["PkgBenchmark", "BenchmarkTools", "JLD2", "DataFrames",
+                            "ArgParse", "Git", "GitHub", "JSON", "Plots", "SolverBenchmark"]
+
+function setup_benchmarks(api::GitHub.GitHubWebAPI, org::String, repositories::Vector{Repo}, new_branch_name::String, base_branch_name::String, message::String; kwargs...)
+    file_paths = [normpath("push_benchmarks.sh"), normpath("benchmark", "run_benchmarks.jl"),
+    normpath("benchmark", "send_comment_to_pr.jl"), normpath("Jenkinsfile")]
+    for repository in repositories
+        clone_repo(repository)
+        create_branch(api, org, repository, new_branch_name, base_branch_name; kwargs...)
+        println(pwd())
+        populate_environment(repository, new_branch_name)
+        println(pwd())
+        rm(joinpath(@__DIR__, "..",repository.name); force = true, recursive = true)
+        println(pwd())
+        Pkg.activate("..")
+    end
+    [update_file(api, file_path, repositories, new_branch_name, "adding/updating file: $file_path"; kwargs...) for file_path in file_paths]
+    create_pullrequests(api, org, repositories, new_branch_name, base_branch_name, message; auth = myauth)
+end
+
+function clone_repo(repository::Repo)
+    clone_url = get_clone_url(repository)
+    try
+        git() do git
+            run(`$git clone $(repository.clone_url)`)
+        end
+    catch exception
+        println("The repository already exists locally!")
+        println(exception)
+    end
+end
+
+function populate_environment(repository::Repo, new_branch_name::String)
+    cd(joinpath(repository.name))
+    git() do git
+        run(`$git pull origin`)
+        run(`$git checkout $new_branch_name`)
+    end
+    Pkg.activate(joinpath("benchmark"))
+    Pkg.add(bmark_dependencies)
+    Pkg.update()
+    Pkg.instantiate()
+    Pkg.resolve()
+    try
+        git() do git
+            run(`$git add benchmark/Project.toml`)
+            run(`$git commit -m "setting up project.toml for benchmarks"`)
+            run(`$git pull`)
+            run(` $git push`)
+        end
+    catch exception
+        println("Working tree clean, nothing to commit!")
+    end
+    cd(joinpath(".."))
+end
+
+function get_clone_url(repository::Repo)
+    credentials = ENV["JSO_GITHUB_AUTH"]
+    url = split("$(repository.clone_url)", "https://")
+
+    return "https://" * credentials * "@" * url[2]
+end
