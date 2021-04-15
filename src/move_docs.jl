a612db637ff0a77ba9415a9158b1539085e97879
Monssaf Toukal <tmonssaf@gmail.com>
2021-04-15 15:24:03 -0400

    updating move docs

--- src/move_docs.jl
+++ src/move_docs.jl
@@ -11,17 +11,59 @@ include(joinpath(@__DIR__,"webhooks.jl"))
 include(joinpath(@__DIR__, "repositories.jl"))
 
 api = GitHub.DEFAULT_API
-# Need to add GITHUB_AUTH to your .bashrc
+# org = "JuliaSmoothOptimizers"
+org = "ProofOfConceptForJuliSmoothOptimizers"
 myauth = GitHub.authenticate(ENV["JSO_GITHUB_AUTH"])
 
 repositories = GitHub.repos(api, org; auth = myauth)[1]
 
 function update_docs(api, org, repositories; kwargs...)
     for repo in repositories
+        has_change = false
         clone_repo(repo)
-        cd(repo.name) do
-            if()
+        git() do git
+            try
+                run(`$git checkout -b workflows --`)
+            catch
+                run(`$git checkout workflows --`)
+            end
         end
+        cd(repo.name) do
+            if("docs" in readdir())
+                cd("docs") do 
+                    if("index.md" in readdir())
+                        run(`mv index.md src/`)
+                        has_change = true
+                        println("index.md moved to src folder")
+                    end
+                    if("tutorial.md" in readdir())
+                        run(`mv tutorial.md src/`)
+                        has_change = true
+                        println("tutorial.md moved to src folder")
+                    end
+                    if("reference.md" in readdir())
+                        run(`mv reference.md src/`)
+                        has_change = true
+                        println("reference.md moved to src folder")
+                    end
+                end
+            else
+                println("docs folder not found in $(repo.name)")
+            end
+            git() do git
+                if(has_change)
+                    run(`$git add docs`)
+                    run(`$git commit -m "fix docs folder structure"`)
+                    try
+                        run(`$git push -u origin workflows`)
+                    catch
+                        run(`$git push origin workflows`)
+                    end
+                    create_pullrequest(api, org, repo, "workflows", "master", "Update CI, TagBot and documentation workflows"; kwargs...)
+                end
+            end
+        end
+        rm(joinpath(@__DIR__, "..", repo.name); force = true, recursive = true)
     end
-
 end
+update_docs(api, org, repositories; auth=myauth)
