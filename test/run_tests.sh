faf39f966d3e4f1e9dacde8522374dd8814e5635
MonssafToukal <tmonssaf@gmail.com>
2022-10-13 18:39:06 -0400

    rm module name env var

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Specify the module to test (e.g "HSL")
-julia -E 'using Pkg; module_name = ENV["moduleName"]; Pkg.activate("test_env"); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.test(module_name)' &> "$org"_"$repo"_"$pullrequest".txt
+julia -E 'using Pkg; module_name = split(ENV["repo"], ".")[1]; Pkg.activate("test_env"); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.test(module_name)' &> "$org"_"$repo"_"$pullrequest".txt
 
 # Create the gist and create comment on PR:
 julia BenchmarkSetup/test/send_gist_url.jl
+
+
+git clean -fd
+git reset --hard
+rm -rf BenchmarkSetup*
