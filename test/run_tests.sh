8aa17bea82b8b94af7d8d06643109aab25c4a1f9
MonssafToukal <tmonssaf@gmail.com>
2022-10-24 22:48:11 -0400

    making jenkins unit tests not fail silently

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+julia --project=BenchmarkSetup BenchmarkSetup/benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**Starting unit tests!**'
+
 # Specify the module to test (e.g "HSL")
 julia -E 'using Pkg; module_name = split(ENV["repo"], ".")[1]; Pkg.activate("test_env"); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.test(module_name)' &> "$org"_"$repo"_"$pullrequest".txt
 
 # Create the gist and create comment on PR:
 julia BenchmarkSetup/test/send_gist_url.jl
+exit_status=$?
 
+if [ $exit_status -ne 0 ] ; then
+    ERROR_LOGS="/home/jenkins/tests/$org/$repo/${pullrequest}_${BUILD_NUMBER}_error.log"
+    julia --project=BenchmarkSetup BenchmarkSetup/benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c "**An error occured while running the unit tests: **" -g $ERROR_LOGS
+fi
 
 git clean -fd
 git reset --hard
