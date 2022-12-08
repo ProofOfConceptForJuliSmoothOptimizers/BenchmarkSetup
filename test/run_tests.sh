cda9a7902f1afc089ebe4d5db8c7013c83b828a9
MonssafToukal <32516773+MonssafToukal@users.noreply.github.com>
2022-12-08 16:04:34 -0500

    Add VersionInfo in logs for unit tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,7 +4,7 @@ julia --project=BenchmarkSetup -E 'using Pkg; Pkg.update(); Pkg.resolve(); Pkg.i
 julia --project=BenchmarkSetup BenchmarkSetup/benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**Starting unit tests!**'
 
 # Specify the module to test (e.g "HSL")
-julia -E 'using Pkg; module_name = split(ENV["repo"], ".")[1]; Pkg.activate("test_env"); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.test(module_name)' &> "$org"_"$repo"_"$pullrequest".txt
+julia -E 'versioninfo(); using Pkg; module_name = split(ENV["repo"], ".")[1]; Pkg.activate("test_env"); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.test(module_name)' &> "$org"_"$repo"_"$pullrequest".txt
 
 # Create the gist and create comment on PR:
 julia BenchmarkSetup/test/send_gist_url.jl
